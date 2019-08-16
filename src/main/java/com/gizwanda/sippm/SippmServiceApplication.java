423b60163904f6d4eb46a009c16dc50a1a9a69bb
Gizwanda <adigiz@yahoo.com>
2019-08-16 13:39:09 +0700

    initial commit

---
+++ src/main/java/com/gizwanda/sippm/SippmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gizwanda.sippm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SippmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SippmServiceApplication.class, args);
+    }
+
+}
