e3043658247c3f18493131b7d4ccb25cbd90ff66
Gizwanda <adigiz@yahoo.com>
2019-09-17 20:32:30 +0700

    [Giz] remove unused import

--- src/main/java/com/gizwanda/sippm/pengumuman/PengumumanController.java
+++ src/main/java/com/gizwanda/sippm/pengumuman/PengumumanController.java
@@ -2,7 +2,6 @@ package com.gizwanda.sippm.pengumuman;
 
 import com.gizwanda.sippm.pengumuman.model.Pengumuman;
 import com.gizwanda.sippm.pengumuman.model.PengumumanDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
